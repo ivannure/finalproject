6cd74608d40d2da0fda9578cebd1898670caa7a5
Ivan Nur Efendi <nurpendi180@gmail.com>
2022-12-08 11:17:31 +0700

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,12 @@
   new PureCounter();
 
 })()
+
+	document.addEventListener("DOMContentLoaded", function(){
+        /////// Prevent closing from click inside dropdown
+        document.querySelectorAll('.dropdown-menu').forEach(function(element){
+        	element.addEventListener('click', function (e) {
+        		e.stopPropagation();
+        	});
+        })
+    }); 
